e45d3421e9897eff5e3b08447dd564bac82933a1
unknown <robertma.green@gmail.com>
2022-09-29 14:52:39 +0100

    100*

--- MoneyDispenserApp/MoneyDispenserApp/Program.cs
+++ MoneyDispenserApp/MoneyDispenserApp/Program.cs
@@ -9,6 +9,7 @@ internal class Program
 
     public static string Money(double input)
     {
+        input = input * 100;
         int c2Pound = 0;
         int c1Pound = 0;
         int c20P = 0;
@@ -17,44 +18,39 @@ internal class Program
         int c1P = 0;
         while (input > 0)
         {
-            if (input > 2)
+            if (input > 200)
             {
                 c2Pound++;
-                input -= 2;
+                input -= 200;
              
             }
-            else if (input > 1)
+            else if (input > 100)
             {
                 c1Pound++;
-                input -= 1;
+                input -= 100;
 
             }
-            else if (input > 0.2)
+            else if (input > 20)
             {
                 c20P++;
-                input -= 0.2;
+                input -= 20;
              
             }
-            else if (input > 0.05)
+            else if (input > 5)
             {
                 c5P++;
-                input -= 0.05;
-              
-
+                input -= 5;
             }
-            else if (input > 0.02)
-            {
-                Console.WriteLine(Math.Round(input, 2)); 
+            else if (input > 2)
+            { 
                 c2P++;
-                input -= 0.02;
+                input -= 2;
                 Console.WriteLine(input);
-                
             }
             else
             {
-                Console.WriteLine(Math.Round(input,2));
                 c1P++;
-                input = 0;
+                input -=1;
             }
         }
         Console.WriteLine($"£2: {c2Pound}, £1: {c1Pound}, 20p:{c20P}, 5p:{c5P}, 2p:{c2P}, 1p:{c1P}");
